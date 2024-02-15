32dc53c4b6d135feead9c2d94bfba281e3f04325
Divesh-dev <158588737+Divesh0072@users.noreply.github.com>
2024-02-16 00:48:44 +0530

    Update DbConfig.js for my project

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.cluster-cx202ms4ebqe.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'root1234',
+    DB_DATABASE : 'webappd'
 });
